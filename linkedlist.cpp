63c01beb6900ee1e2f4eb920639417e5b6060fa5
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-08 16:35:44 +0700

    Menambahkan prosedur addNote

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -19,4 +19,42 @@ class List
 
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan nomor mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START -> noMhs)
+        {
+            if ((START != NULL ) && (nim == START ->noMhs))
+            {
+                cout << "\nDuplikasi  noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;    
+        }
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL)&&(nim >= current ->noMhs))
+        {
+            if (nim= current -> noMhs)
+            {
+                cout << "\nDuplikasi  noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            previous = current -> next;  
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
 };
