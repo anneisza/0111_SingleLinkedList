0a2f7fbcbf46ae55a23723cf0748d63d0e6c80d8
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-08 16:37:34 +0700

    Menambahkan fungsi Search

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -62,4 +62,17 @@ class List
     {
         return (START == NULL);
     }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL)&&(nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
 };
